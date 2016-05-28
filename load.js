42ab8ef1b6a55e64a8b0f7e2a4514cd4f8ba58bc
Matt Deeds <mdeeds@gmail.com>
2016-05-28 12:50:34 -0700

    Factor out load logic

--- load.js
+++ load.js
@@ -1,13 +1,25 @@
 
+var Load = function() {}
 
 
-Load.
+Load.step1_ = function(block, on_load_body) {
+  var bodies = [];
 
-
-Load.step1_ = function(data_so_far) {
-
+  $.each(block, function(key, val) {
+    console.log('Object name: ' + key);
+    var data = val.data;
+    var body = new Body();
+    for (var i = 0; i < data.length; ++i) {
+      body.addPosition(data[i]);
+    }
+    bodies.push(body);
+  });
+ 
+  for (var i = 0; i < bodies.length; ++i) {
+    on_load_body(bodies[i]);
+  }
 }
 
-Load.load = function() {
-
+Load.load = function(source_uri, on_load_body) {
+  $.getJSON(source_uri, function(block) {Load.step1_(block, on_load_body);});
 }
